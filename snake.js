8054412ddac77cc883098f2a3ca64abdbcfa34cb
Karl Xu <karl.yogi.xu@gmail.com>
2019-12-12 01:43:33 +0800

    move object upward

--- snake.js
+++ snake.js
@@ -2,14 +2,26 @@ console.log('snake')
 
 var example = document.getElementById('example');
 var context = example.getContext('2d');
+
+// let START_X=30
+// let START_Y=30
+let anchor = {
+    x: 30,
+    y: 30
+}
 context.fillStyle = 'red';
-context.fillRect(30, 30, 50, 50);
+context.fillRect(anchor.x, anchor.y, 30, 30);
 
 
-setTimeout(function() {
-    // alert('test')
-    context.fillStyle = 'green';
-    context.fillRect(30, 30, 50, 50);
-
-}, 1000)
-
+document.onkeydown = function (e) {
+    // e = e || window.event;
+    console.log(e)
+    // use e.keyCode
+    if(e.key == "ArrowUp") {
+        context.fillStyle = 'white';
+        context.fillRect(anchor.x, anchor.y, 30, 30);
+        anchor.y -=5
+        context.fillStyle = 'red';
+        context.fillRect(anchor.x, anchor.y, 30, 30);
+    }
+};
